e948497e07c72ab54aaa67977ae2118af4ceff89
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-02 04:28:20 +0200

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,7 +1,7 @@
 import urllib.parse
 
-encoded_code = '''HERE''' # <--- encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a%3c%68%74'''
-decoded_code = urllib.parse.unquote(encoded_code)
+encrypted = '''HERE''' # <--- encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a%3c%68%74'''
+decrypt = urllib.parse.unquote(encrypted)
 
-print(decoded_code)
+print(decrypt)
 input()
