64c860fbeea1069c46af7a126378c20b68aaf277
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-14 23:26:20 +0200

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,7 +1,10 @@
 import urllib.parse
 import os
-encrypted = '''HERE''' # <-- Your encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a'''
+
+encrypted = ''' HERE ''' # <-- encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a'''
+
 decrypt = urllib.parse.unquote(encrypted)
+
 path = 'decrypted.html'
 with open(path, 'w') as file:
     file.write(decrypt)
