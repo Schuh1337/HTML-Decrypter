0db6aef9400dc91784875af92fa32cff10907942
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-10-14 23:22:32 +0200

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -2,7 +2,7 @@ import urllib.parse
 import os
 encrypted = '''HERE''' # <-- Your encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a'''
 decrypt = urllib.parse.unquote(encrypted)
-path = 'decrypted.txt'
+path = 'decrypted.html'
 with open(path, 'w') as file:
     file.write(decrypt)
 print(f"Decrypted code has been saved to {os.path.abspath(path)}")
