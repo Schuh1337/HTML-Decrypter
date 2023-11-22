2a82816ac94ae4a72e72abf2c1891d35c04fc881
Schuh <141640357+Schuh1337@users.noreply.github.com>
2023-11-22 19:46:12 +0100

    Update decrypter.py

--- decrypter.py
+++ decrypter.py
@@ -1,5 +1,4 @@
-import urllib.parse
-import os
+import urllib.parse, os
 
 encrypted = ''' HERE ''' # <-- encrypted string here | example: '''%3c%21%44%4f%43%54%59%50%45%20%68%74%6d%6c%3e%0d%0a'''
 
